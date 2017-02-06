866b9781f803bba8796888b8c4b03699c16eeb24
shijh <shijinhui90@sina.com>
2017-02-06 15:08:18 +0800

    add menu.h

---
+++ menu.h
@@ -0,0 +1,11 @@
+#ifndef __MENU_H__
+#define __MENU_H__
+
+#define DISPLAY_WIDTH			800
+#define DISPLAY_HIGHT			600
+#define LENGTH_MAX_FILENAME		256
+#define PIC_WIDTH_MAX			128
+#define PIC_HIGHT_MAX			128
+
+#endif
+
